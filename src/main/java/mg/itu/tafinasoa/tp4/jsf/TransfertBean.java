f259dc665ddad59b791aa1fbc6bf97d32b39aacb
ace34TT <tafinasoa35@gmail.com>
2024-04-14 13:46:45 +0300

    Error handling

--- src/main/java/mg/itu/tafinasoa/tp4/jsf/TransfertBean.java
+++ src/main/java/mg/itu/tafinasoa/tp4/jsf/TransfertBean.java
@@ -7,6 +7,10 @@ package mg.itu.tafinasoa.tp4.jsf;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+import mg.itu.tafinasoa.tp4.entity.CompteBancaire;
+import mg.itu.tafinasoa.tp4.jsf.util.Util;
 import mg.itu.tafinasoa.tp4.service.GestionnaireCompte;
 
 /**
@@ -19,19 +23,44 @@ public class TransfertBean {
 
     private Long idCompteSource;
     private Long idCompteDestination;
-    private int montant;
+    private Integer montant;
+
+    List<String> erreurs = new ArrayList<>();
 
     /**
      * Creates a new instance of TransfertBean
      */
     public TransfertBean() {
+
     }
 
     @Inject
     private GestionnaireCompte gestionnaireCompte;
 
     public String effectuerTransfert() {
+        CompteBancaire compteSource = gestionnaireCompte.trouverCompteParId(idCompteSource);
+        CompteBancaire compteDestination = gestionnaireCompte.trouverCompteParId(idCompteDestination);
+
+        if (compteSource == null) {
+            erreurs.add("Le compte source n'existe pas.");
+        }
+
+        if (compteDestination == null) {
+            erreurs.add("Le compte destination n'existe pas.");
+        }
+
+        if (compteSource != null && compteDestination != null && compteSource.getSolde() < montant) {
+            erreurs.add("Solde insuffisant dans le compte source.");
+        }
+
+        if (!erreurs.isEmpty()) {
+            for (String erreur : erreurs) {
+                Util.messageErreur(erreur);
+            }
+            return null; // Rester sur la même page pour corriger les erreurs
+        }
         gestionnaireCompte.effectuerTransfert(idCompteSource, idCompteDestination, montant);
+        Util.addFlashInfoMessage("Transfert effectué avec succès");
         return "listeComptes?faces-redirect=true";
     }
 
@@ -51,11 +80,11 @@ public class TransfertBean {
         this.idCompteDestination = idCompteDestination;
     }
 
-    public int getMontant() {
+    public Integer getMontant() {
         return montant;
     }
 
-    public void setMontant(int montant) {
+    public void setMontant(Integer montant) {
         this.montant = montant;
     }
 
